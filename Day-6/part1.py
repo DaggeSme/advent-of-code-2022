7cbc44510779fb97f0e4ac7ca568cea440c30afc
DaggeSme <dag.smedberg@gmail.com>
2022-12-07 19:44:19 +0100

    Update Day6

--- Day-6/part1.py
+++ Day-6/part1.py
@@ -8,23 +8,21 @@ def dup(somelist):
     no = False
     e = range(0,len(somelist))
     for times in e:
-        #print(times)
-        i=len(somelist)-1
-        while i < len(somelist)-1:
-            i+=1
-            print("times=",times)
-            print("i=",i)
-            print(somelist[times],somelist[i])
+        i=0+times+1
+        while i < len(somelist):
             if somelist[times] == somelist[i]:
                 yes = True
             else:
                 no = True
+            i+=1
     if yes == False and no == True:
-        return False
-    else:
         return True
+    else:
+        return False
+
 
 q = range(0,len(contents))
+done = False
 for times in q:
     if done == False:
         List = []
@@ -32,6 +30,30 @@ for times in q:
         List.append(contents[times+1])
         List.append(contents[times+2])
         List.append(contents[times+3])
-        if dup(List) != True:
+        if dup(List) == True:
             done = True
+            print("Part1:")
             print(times+4)
+q = range(0,len(contents))
+done = False
+for times in q:
+    if done == False:
+        List = []
+        List.append(contents[times])
+        List.append(contents[times+1])
+        List.append(contents[times+2])
+        List.append(contents[times+3])
+        List.append(contents[times+4])
+        List.append(contents[times+5])
+        List.append(contents[times+6])
+        List.append(contents[times+7])
+        List.append(contents[times+8])
+        List.append(contents[times+9])
+        List.append(contents[times+10])
+        List.append(contents[times+11])
+        List.append(contents[times+12])
+        List.append(contents[times+13])
+        if dup(List) == True:
+            done = True
+            print("Part2:")
+            print(times+14)
